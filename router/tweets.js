af5f7dfea2933e9425131c5e656aae93ff4e712f
refresh88 <ouok88@gmail.com>
2023-04-07 00:05:37 +0900

    tweet
    express node 서버 구현.
    tweet api 작성

---
+++ router/tweets.js
@@ -0,0 +1,76 @@
+import express from 'express';
+import 'express-async-errors';
+
+let tweets = [
+  {
+    id: '1',
+    text: '드림코딩 화이팅!',
+    createdAt: Date.now().toString(),
+    name: 'Bob',
+    username: 'bob',
+    url: 'https://w7.pngwing.com/pngs/182/1015/png-transparent-computer-icons-child-man-child-face-child-face-smiley.png',
+  },
+  {
+    id: '1',
+    text: 'ellie 화이팅!',
+    createdAt: Date.now().toString(),
+    name: 'Ellie',
+    username: 'ellie',
+  },
+];
+const router = express.Router();
+
+// GET /tweets
+// GET /tweets?username=:username
+router.get('/', (req, res, next) => {
+  // 유저명이 있을때 / 없을때
+  const username = req.query.username;
+  const data = username
+    ? tweets.filter((tweet) => tweet.username === username)
+    : tweets;
+  res.status(200).json(data);
+});
+// GET /tweets/:id
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+// POST /tweets
+router.post('/', (req, res, next) => {
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
+});
+// PUT /teets/:id
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+
+// DELETE /tweets/:id
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
+});
+
+export default router;
